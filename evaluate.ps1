dc8273ac512c2342ee4129532b62999ae2565f9f
gitookie <1461332809@qq.com>
2026-03-09 18:32:01 +0800

    move the evaluate script to the scripts/test folders

--- evaluate.ps1
+++
@@ -1,5 +0,0 @@
-python .\tests\unified_evaluate_hotpotqa.py --models graph_rag `
---graphrag_work_dir "D:\Develop\all_RAG\routing_rag\graphrag_index_hotpotqa_train_5000_samples" `
---num_samples 5000 `
---skip_graphrag_index `
---hotpotqa_file "D:\Develop\all_RAG\routing_rag\HotpotQA\hotpot_train_v1.1_5000_samples.jsonl"
