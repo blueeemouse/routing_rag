33b95c11b8e9136e75b36a89bae0ce315258dec2
blueeemouse <1461332809@qq.com>
2026-03-24 10:18:08 +0800

    add test data of GraphRAG class, deployed by vllm

--- scripts/test/evaluate.sh
+++ scripts/test/evaluate.sh
@@ -6,7 +6,7 @@
 python ./tests/unified_evaluate_hotpotqa.py \
     --models graph_rag \
     --graphrag_work_dir "/home/lhz/code/routing_rag/graphrag_ollama_hotpotqa_1000_test_data" \
-    --num_samples 2 \
+    --num_samples 1000 \
     --skip_graphrag_index \
     --delay 0 \
-    --hotpotqa_file "/home/lhz/code/routing_rag/HotpotQA/hotpot_train_v1.1_5000_samples.jsonl"
+    --hotpotqa_file "/home/lhz/code/routing_rag/HotpotQA/hotpot_dev_distractor_1000_samples.jsonl"
